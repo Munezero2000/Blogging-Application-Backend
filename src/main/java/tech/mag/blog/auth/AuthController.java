bbc0f04b0b503481aa672865848d01b7c9a13f28
Mag codes <muneagechaste@gmail.com>
2024-05-21 09:30:39 +0200

    resolved merge conflict on main the main branch

--- src/main/java/tech/mag/blog/auth/AuthController.java
+++ src/main/java/tech/mag/blog/auth/AuthController.java
@@ -37,7 +37,9 @@ public class AuthController {
     private JwtService jwtService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<Map<String, String>> authenticateUser(@Valid @RequestBody LoginRequest loginRequest,
+            HttpServletResponse response) {
+        Map<String, String> responseBody = new HashMap<>();
         try {
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
@@ -48,15 +50,79 @@ public class AuthController {
             User theUser = user.orElse(null);
             final String token = jwtService.generateToken(userDetails);
 
-            Map<String, String> response = new HashMap<>();
-            response.put("token", token);
+            // Create a cookie with the token
+            Cookie cookie = new Cookie("token", token);
+            cookie.setHttpOnly(true);
+            cookie.setSecure(true);
+            cookie.setPath("/");
+            cookie.setMaxAge(24 * 60 * 60);
+
+            response.addCookie(cookie);
+
+            responseBody.put("message", "Login successful");
             if (theUser != null) {
                 response.put("userId", theUser.getId().toString());
             }
-            return ResponseEntity.ok(response);
+            return ResponseEntity.ok(responseBody);
+        } catch (BadCredentialsException e) {
+            responseBody.put("error", "Invalid email or password");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+        } catch (UsernameNotFoundException e) {
+            responseBody.put("error", "User not found");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error");
+        }
+    }
+
+    @PostMapping(value = "/register", consumes = { MediaType.APPLICATION_JSON_VALUE }, produces = {
+            MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<?> registerUser(
+            @Valid @RequestBody UserCreationRequest creationUser) {
+        try {
+
+            User user = new User();
+            user.setNames(creationUser.getUsername());
+            user.setEmail(creationUser.getEmail());
+
+            // Hashing the password
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            String hashedPassword = passwordEncoder.encode(creationUser.getPassword());
+            user.setPassword(hashedPassword);
+
+            User theUser = userService.registerUser(user);
+
+            if (theUser == null) {
+                return new ResponseEntity<>("User with email: " + creationUser.getEmail() + " already exists",
+                        HttpStatus.BAD_REQUEST);
+            } else {
+                String to = user.getEmail();
+
+                String subject = "Account created for " + user.getUsername();
+
+                String htmlContent = emailService.renderHtmlTemplate(
+                        "Account Creation Successful!",
+                        "<p>Dear " + user.getNames() + ",</p>" +
+                                "<p>We are thrilled to confirm that your account has been successfully created. You can now enjoy the following features on our platform:</p>"
+                                +
+                                "<ul>" +
+                                "<li>Commenting on posts</li>" +
+                                "<li>Liking and sharing posts</li>" +
+                                "<li>Authoring your own blog posts</li>" +
+                                "</ul>" +
+                                "<p>We are excited to have you join our community. Feel free to <a href='http://localhost:8081/swagger-ui/index.html'>explore more</a> and start sharing your ideas.</p>"
+                                +
+                                "<p>Cheers,</p>" +
+                                "<p>The Paty Blogging Team</p>",
+                        "https://magbrand.netlify.app/blog-pages/blog");
+
+                emailService.sendHtmlEmail(to, subject, htmlContent);
+                return new ResponseEntity<>(theUser, HttpStatus.OK);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
